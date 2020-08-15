e0cf4120c192aa594cc766da51a44c580cb736b7
Đinh Anh Huy <dinhanhhuy.it@gmail.com>
2020-08-15 19:20:40 +0700

    update

--- wsync.sh
+++ wsync.sh
@@ -1,20 +1,5 @@
 set -e
 
-# Setting
-HOUR_BEFORE_CHECK=3
-WSYNC_CONFIG_FILE=~/.wsync.config
-IGNORE_CACHE_TIME=true
-AUTO_UPDATE=true
-USER_PATH="/root"
-BASE_CONFIG_PATH="$USER_PATH/.zshrc"
-SHARE_CONFIG_PATH=""
-GIT_FOLDER="$USER_PATH/.ssh"
-
-if [ ! -f $WSYNC_CONFIG_FILE ]; then
-  echo "[INFO]: Create config file"
-  touch $WSYNC_CONFIG_FILE
-fi
-
 ask() {
   read -p "$1" ASK_CONFIRM && [[ $ASK_CONFIRM == [yY] || $ASK_CONFIRM == [yY][eE][sS] ]]
 }
@@ -27,30 +12,47 @@ update() {
   if [ $AUTO_UPDATE ] || [ $ASK_CONFIRM ];  then
     git pull
     date +%s > $WSYNC_CONFIG_FILE
-    echo "[INFO]: $ source $BASE_CONFIG_PATH"
-    source $BASE_CONFIG_PATH
   fi
 }
 
 check_for_update() {
+  if [ ! -f $WSYNC_CONFIG_FILE ]; then
+    touch $WSYNC_CONFIG_FILE
+  fi
   LAST=$(cat $WSYNC_CONFIG_FILE)
   LAST=${LAST:-0}
   NOW=$(date +%s)
-  # echo $LAST $NOW
   SECOND=$(($NOW-$LAST))
   HOUR=$((DELTA/3600))
 
   if [ ! $IGNORE_CACHE_TIME ] && [ $HOUR -lt $HOUR_BEFORE_CHECK ]; then
     exit 0
   fi
-
+  
   cd $GIT_FOLDER
   CURRENT_COMMIT=$(git rev-parse HEAD)
   LATEST_COMMIT=$(git rev-parse origin/master)
 
   if [ "$CURRENT_COMMIT" = "$LATEST_COMMIT" ]; then
     update
+    if [ "$CUSTOM_EVAL" ]; then
+      eval $CUSTOM_EVAL
+    fi
   fi
 }
 
-check_for_update
+# Let's rock
+for FILE in config/*; do
+  (
+    echo "[INFO]: start sync config $FILE"
+    while read line; do
+      eval $line
+    done < $FILE
+    if [ -f $WSYNC_CONFIG_FILE ]; then
+      CONFIG_NAME=${FILE##*/}
+      CACHE_PATH=$(eval echo ~$USER)
+      WSYNC_CONFIG_FILE=${WSYNC_CONFIG_FILE:-"$CACHE_PATH/.wsync.$CONFIG_NAME.time"}
+    fi
+    check_for_update
+  )
+done  
